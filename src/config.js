1ac9a5f3118964248e6fb66b5ef834fe93e589c8
Trevor Alt <trevorjalt@gmail.com>
2020-11-16 01:15:33 -0500

    deployment fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,15 @@
+let apiPath
+let tokenKey
+
+if (process.env.NODE_ENV === 'production') {
+    apiPath = process.env.API_ENDPOINT
+    tokenKey = process.env.TOKEN_KEY
+} else {
+    apiPath = 'http://localhost:8000/api'
+    tokenKey = process.env.TOKEN_KEY
+}
+
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
-    TOKEN_KEY: process.env.TOKEN_KEY
+    API_ENDPOINT: apiPath,
+    TOKEN_KEY: tokenKey,
 }
